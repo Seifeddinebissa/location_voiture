5bda9c6ec6ee8640775f8ebc04c3ff1cae319a56
Jamila Ben Abdelkader <Benabdeljamyla@gmail.com>
2025-02-19 20:27:31 +0100

    paiementController added

---
+++ Microservices/GestionPaiement/src/main/java/tn/esprit/gestionpaiement/Entities/Paiement.java
@@ -0,0 +1,39 @@
+package tn.esprit.gestionpaiement.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Paiement {
+    @Id
+    @GeneratedValue
+    private Long idPaiement;
+    private  Double montant;
+    private  Mode mode;
+    private  boolean traited;
+
+    public Paiement() {}
+    public Paiement(Double montant, Mode mode, boolean traited) {
+        this.idPaiement=idPaiement; this.montant = montant;this.mode = mode;
+        this.traited = traited;}
+
+    public Long getIdPaiement() {return idPaiement;}
+
+    public void setIdPaiement(Long idPaiement) {this.idPaiement = idPaiement;}
+
+    public Double getMontant() {return montant;}
+
+    public void setMontant(Double montant) {this.montant = montant;}
+
+    public Mode getMode() {return mode;}
+
+    public void setMode(Mode mode) {this.mode = mode;}
+
+    public boolean isTraited() {return traited;}
+
+    public void setTraited(boolean traited) {this.traited = traited;}
+
+}
---
+++ Microservices/GestionPaiement/src/test/java/tn/esprit/gestionpaiement/GestionPaiementApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionpaiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPaiementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

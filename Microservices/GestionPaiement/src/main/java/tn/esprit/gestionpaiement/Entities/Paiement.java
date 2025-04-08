bb90f43afd8bd9b847897f1ffb316a372c97887a
Jamila Ben Abdelkader <Benabdeljamyla@gmail.com>
2025-04-08 09:42:39 +0100

    gestion paiement

--- Microservices/GestionPaiement/src/main/java/tn/esprit/gestionpaiement/Entities/Paiement.java
+++ Microservices/GestionPaiement/src/main/java/tn/esprit/gestionpaiement/Entities/Paiement.java
@@ -3,10 +3,15 @@ package tn.esprit.gestionpaiement.Entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+
+
 public class Paiement {
     @Id
     @GeneratedValue
--- Microservices/GestionPaiement/src/main/java/tn/esprit/gestionpaiement/Repositories/PaiementRepository.java
+++ Microservices/GestionPaiement/src/main/java/tn/esprit/gestionpaiement/Repositories/PaiementRepository.java
@@ -1,11 +1,12 @@
 package tn.esprit.gestionpaiement.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.gestionpaiement.Entities.Paiement;
 
 import java.util.List;
-
+@Repository
 public interface PaiementRepository extends JpaRepository<Paiement,Long> {
-
- //   List<Paiement> getPaiementById(Long idPaiement);
+    // Méthode personnalisée pour trouver les paiements non traités
+    List<Paiement> findByTraitedFalse();
 }

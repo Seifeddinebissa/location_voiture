ce257f42a88a576a3bc7f7295c251ce065450e27
arij <arij.abdellaoui@esprit.tn>
2025-04-11 16:59:19 +0100

    gestion reservation update2

--- GestionReservation/src/main/java/tn/esprit/gestionreservation/repositories/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.gestionreservation.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.gestionreservation.entities.Reservation;
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}

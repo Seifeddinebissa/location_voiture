0047e11465a2b3a628187fecae4f8f6642f9deb0
arij <arij.abdellaoui@esprit.tn>
2025-04-18 19:10:30 +0100

    Updatee

--- Microservices/GestionVehicule/src/main/java/com/example/microservice/gestionvehicule/repository/VehiculeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.microservice.gestionvehicule.repository;
-
-import com.example.microservice.gestionvehicule.entities.Vehicule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehiculeRepository  extends JpaRepository<Vehicule,Long> {
-}
